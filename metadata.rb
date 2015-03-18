f3970bf51f95c20171597619ba6597e9d1009ed9
Eric Saxby <eric@wanelo.com>
2015-03-18 15:12:48 -0700

    0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ops@wanelo.com'
 license          'MIT'
 description      'Installs sequel-schema-sharding-minotaur for histograms of shard access'
 long_description 'Installs sequel-schema-sharding-minotaur for histograms of shard access'
-version          '0.1.0'
+version          '0.2.0'
 
 depends 'bundler' # see Berksfile
 depends 'smf'
